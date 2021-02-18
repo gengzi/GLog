d940374cebe9624371c2addd8602146144b26667
gengzi <1164014750@qq.com>
2021-02-18 23:15:51 +0800

    [update]增加类测试方法

---
+++ Glog-test/src/main/java/fun/gengzi/test/classshow/GlogTest.java
@@ -0,0 +1,19 @@
+package fun.gengzi.test.classshow;
+
+public class GlogTest {
+    private int arg1 = 1;
+
+    public final static String INFO = "NIUPI";
+
+    /**
+     * 求和
+     * @param a 参数a
+     * @param b 参数b
+     * @return a+b
+     */
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+
+}
